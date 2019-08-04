5bc17acfc1a47eabae81dd42c601b44307eb448f
Ricardo Margalhau <kryfor@outlook.com>
2019-08-04 17:17:50 +0100

    new website

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "https://api.github.com/users/xploitedd/repos",
+        contentType: "application/json"
+    }).done(function(data) {
+        data.forEach(repo => {
+            console.log(repo.name.length)
+            let name = repo.name
+            if (name.length > 20)
+                name = name.substring(0, 17) + "..."
+
+            $("#github-projects")
+                .append('<a class="repo" href="' + repo.html_url + '"><div class="block-item"><h2 class="title">' + name + 
+                        '</h2><span class="language ' + repo.language.toLowerCase().replace('#', 'sharp') + '">' + repo.language + 
+                        '</span>' + (repo.description != null ? '<p class="description">' + repo.description + '</p>' : '') + '</div></a>')
+        });
+    })
+})
